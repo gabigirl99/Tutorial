2ef211c850a4b27548a23d8416af456d0b4e0ffc
gabigirl99 <gabigirl99@comcast.net>
2015-01-07 16:50:01 -0500

    Create GabriellaMontague1.py

---
+++ GabriellaMontague1.py
@@ -0,0 +1,23 @@
+#Gabriella#
+#titles#
+import os
+import sys
+#===#
+#questionaire#
+#===#
+q1=raw_input"Hi, how are you?"
+q2=raw_input"When did you join robotics?"
+q3=raw_input"What made you want to join robotics?"
+q4=raw_input"What's your favorite part of robotics?"
+q5=raw_input"When did you pass the level of being a noob?"
+q6=raw_input"Do you design codes for video games?"
+q7=raw_input"If so, what kind of games?"
+q8=input"2+4"
+q9=input"4+4"
+print"Have a nice day."
+if q5=="sophomore year":
+  print"Can't wait!"
+else:
+  print"Woohoo!" 
+elif q8=="6" while q9=="8"
+  print "Good Job!"
